04d26ccd2741da8bb0ebbfcdde1888f28efa2078
Sergey Mironov <grrwlf@gmail.com>
2020-01-22 16:52:55 +0300

    Change pweave output format to md

--- mkdocs.sh
+++ mkdocs.sh
@@ -6,8 +6,16 @@ if ! which pydoc-markdown >/dev/null 2>&1 ; then
   exit 1
 fi
 
+if ! which pweave >/dev/null 2>&1 ; then
+  echo "pweave not found. Please install it with"
+  echo "> sudo -H pip3 install pweave"
+  exit 1
+fi
+
 pydoc-markdown \
   --modules \
     pylightnix.types pylightnix.core pylightnix.stages \
   --search-path  \
     src /usr/lib/python3.6/ > ./docs/Reference.md
+
+pweave -f markdown docs/demos/MNIST.pmd
