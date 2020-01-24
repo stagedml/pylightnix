5d1d0fd69907323cbebe9cdbddc5ba39f6456d54
Sergey Mironov <grrwlf@gmail.com>
2020-01-24 12:14:08 +0300

    Update fetchurl

--- src/pylightnix/stages/fetchurl.py
+++ src/pylightnix/stages/fetchurl.py
@@ -1,5 +1,7 @@
-from pylightnix.imports import (sha256, urlparse, Popen, remove, basename, join, rename, isfile )
-from pylightnix.types import ( DRef, Manager, Config, Build, Context, Name, Path )
+from pylightnix.imports import (sha256 as sha256sum, urlparse, Popen, remove,
+    basename, join, rename, isfile )
+from pylightnix.types import ( DRef, Manager, Config, Build, Context, Name,
+    Path, Optional)
 from pylightnix.core import ( mkconfig, mkbuild, build_cattrs, build_outpath,
     mkdrv, only, build_wrapper )
 from pylightnix.utils import ( get_executable )
@@ -9,52 +11,58 @@ WGET=get_executable('wget', 'Please install `wget` pacakge')
 AUNPACK=get_executable('aunpack', 'Please install `apack` tool from `atool` package')
 
 
-def config(url:str, sha256:str, mode:str='unpack,remove', name:Name=None)->Config:
-  return mkconfig(locals())
 
-# def downloaded(s:State)->State:
-#   return state_add(s, 'download')
+def fetchurl(m:Manager, url:str, sha256:str, mode:str='unpack,remove', name:Optional[Name]=None)->DRef:
+  """ Download and unpack an URL addess.
 
-def download(b:Build)->None:
-  c=build_cattrs(b)
-  o=build_outpath(b)
+  Downloading is done by calling `wget` application. Optional unpacking is
+  performed with `aunpack` script from `atool` package. `sha256` defines a
+  SHA-256 hashsum of the stored data that should match. `mode` allows to tweak
+  the stage's behavior: adding word 'unpack' instructs fetchurl to unpack the
+  package, 'remove' instructs it to remove the archive after unpacking.  """
 
-  try:
-    fname=basename(urlparse(c.url).path)
-    partpath=join(o,fname+'.tmp')
-    p=Popen([WGET, "--continue", '--output-document', partpath, c.url], cwd=o)
-    p.wait()
-
-    assert p.returncode == 0, f"Download failed, errcode '{p.returncode}'"
-    assert isfile(partpath), f"Can't find output file '{partpath}'"
-
-    with open(partpath,"rb") as f:
-      realhash=sha256(f.read()).hexdigest();
-      assert realhash==c.sha256, f"Expected sha256 checksum '{c.sha256}', but got '{realhash}' instead"
-
-    fullpath=join(o,fname)
-    rename(partpath, fullpath)
-
-    if 'unpack' in c.mode:
-      print(f"Unpacking {fullpath}..")
-      p=Popen([AUNPACK, fullpath], cwd=o)
-      p.wait()
-      assert p.returncode == 0, f"Unpack failed, errcode '{p.returncode}'"
-      if 'remove' in c.mode:
-        print(f"Removing {fullpath}..")
-        remove(fullpath)
-
-    # protocol_add(m, 'download')
-  except Exception as e:
-    print(f"Download failed:",e)
-    print(f"Temp folder {o}")
-    raise
-
-
-def fetchurl(m:Manager, *args, **kwargs)->DRef:
   def _instantiate()->Config:
-    return config(*args, **kwargs)
-  return mkdrv(m, _instantiate, only, build_wrapper(download))
+    name:str=name or 'fetchurl'
+    url:str=url
+    sha256:str=sha256
+    mode:str=mode
+    return mkconfig(locals())
+
+  def _realize(b:Build)->None:
+    c=build_cattrs(b)
+    o=build_outpath(b)
+
+    try:
+      fname=basename(urlparse(c.url).path)
+      partpath=join(o,fname+'.tmp')
+      p=Popen([WGET, "--continue", '--output-document', partpath, c.url], cwd=o)
+      p.wait()
+
+      assert p.returncode == 0, f"Download failed, errcode '{p.returncode}'"
+      assert isfile(partpath), f"Can't find output file '{partpath}'"
+
+      with open(partpath,"rb") as f:
+        realhash=sha256sum(f.read()).hexdigest();
+        assert realhash==c.sha256, f"Expected sha256 checksum '{c.sha256}', but got '{realhash}'"
+
+      fullpath=join(o,fname)
+      rename(partpath, fullpath)
+
+      if 'unpack' in c.mode:
+        print(f"Unpacking {fullpath}..")
+        p=Popen([AUNPACK, fullpath], cwd=o)
+        p.wait()
+        assert p.returncode == 0, f"Unpack failed, errcode '{p.returncode}'"
+        if 'remove' in c.mode:
+          print(f"Removing {fullpath}..")
+          remove(fullpath)
+
+    except Exception as e:
+      print(f"Download failed:",e)
+      print(f"Temp folder {o}")
+      raise
+
+  return mkdrv(m, _instantiate, only, build_wrapper(_realize))
 
 
 
