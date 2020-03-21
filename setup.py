af6a7a6d041470dd9792d9576652cba700f3586e
Sergey Mironov <grrwlf@gmail.com>
2020-03-21 21:08:05 +0300

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   package_dir={'':'src'},
   package_data={"pylightnix": ["py.typed"]},
   zip_safe=False, # https://mypy.readthedocs.io/en/latest/installed_packages.html
-  version="0.0.2",
+  version="0.1.0",
   author="grwlf",
   author_email="grrwlf@gmail.com",
   description="A Nix-style datastore management library in Python",
