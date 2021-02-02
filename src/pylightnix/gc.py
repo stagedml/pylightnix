474ece61dfc2740006ee206c37bd4c480c031846
Sergey Mironov <grrwlf@gmail.com>
2021-02-03 02:36:56 +0300

    Update GC interactive interface

--- src/pylightnix/gc.py
+++ src/pylightnix/gc.py
@@ -35,20 +35,33 @@ def diskspace_h(sz:int)->str:
          f"{sz//2**30:4} G"
 
 
-def gc_exceptions(keep_dir:Path)->Tuple[List[DRef],List[RRef]]:
+def gc_exceptions(keep_paths:List[Path])->Tuple[List[DRef],List[RRef]]:
+  """ Scans `keep_paths` list for references to Pylightnix storage. Ignores
+  unrelated filesystem objects. """
   keep_drefs:List[DRef]=[]
   keep_rrefs:List[RRef]=[]
-  for root, dirs, filenames in walk(keep_dir, topdown=True):
-    for dirname in sorted(dirs):
-      a=Path(abspath(join(root, dirname)))
-      if islink(a):
-        rref=path2rref(a)
-        if rref is not None:
-          keep_rrefs.append(rref)
-        else:
-          dref=path2dref(a)
-          if dref is not None:
-            keep_drefs.append(dref)
+
+  def _check(f:str):
+    nonlocal keep_drefs, keep_rrefs
+    if islink(a):
+      rref=path2rref(a)
+      if rref is not None:
+        keep_rrefs.append(rref)
+      else:
+        dref=path2dref(a)
+        if dref is not None:
+          keep_drefs.append(dref)
+
+  for path in keep_paths:
+    if islink(path):
+      _check(path)
+    elif isdir(path):
+      for root, dirs, filenames in walk(path, topdown=True):
+        for dirname in sorted(dirs):
+          a=Path(abspath(join(root, dirname)))
+          _check(a)
+    else:
+      pass
   return keep_drefs,keep_rrefs
 
 
@@ -61,7 +74,9 @@ def gc_candidates(keep:Tuple[List[DRef],List[RRef]])->Tuple[Set[DRef],Set[RRef]]
   return store_gc(keep_drefs=keep[0], keep_rrefs=keep[1])
 
 
-def gc(keep_dir:Path, interactive:bool=True)->None:
+def gc(keep_dirs:List[Path],
+       interactive:bool=True,
+       verbose:bool=True)->None:
   """ Simple console garbage collector. `gc` removes any model which is not
   symlinked under `keep_dir` and is not in short list of pre-defined models.
 
@@ -70,20 +85,26 @@ def gc(keep_dir:Path, interactive:bool=True)->None:
   import sys
   assert (not interactive) or sys.__stdin__.isatty(), (
     "`gc` needs TTY to be called with `interactive=True`" )
+  assert not (verbose and not interactive), (
+    "gc: `verbose=True` implies `interactive=True`" )
 
-  drefs,rrefs=gc_candidates(gc_exceptions(keep_dir))
+  drefs,rrefs=gc_candidates(gc_exceptions(keep_dirs))
 
-  if len(drefs)+len(rrefs)==0:
-    return
-
-  if interactive:
+  if verbose:
+    total=0
+    if len(drefs)+len(rrefs)>0:
+      print("Objects to be removed:")
     rrefs_pairs=sorted([(rref, dirsize(rref2path(rref))) for rref in rrefs],
              key=lambda x:x[1])
     for dref in drefs:
       print(f"\t{dref}")
     for rref,sz in rrefs_pairs:
       print(f"{diskspace_h(sz)}\t{rref}")
-    print(f"Confirm remove? [n]")
+      total+=sz
+    print(f"{diskspace_h(total)}\ttotal")
+
+  if interactive:
+    print("Confirm removal? [yN]")
     ans=input()
     if ans.lower() != 'y':
       print("Cancelled by user")
