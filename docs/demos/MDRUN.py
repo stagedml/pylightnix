852a997ee1a51af4ca112d164279eba0ad4e8dff
Sergey Mironov <grrwlf@gmail.com>
2021-08-30 01:46:34 +0300

    Update docs/demos

--- docs/demos/MDRUN.py
+++ docs/demos/MDRUN.py
@@ -6,16 +6,18 @@ from pylightnix import (StorageSettings, Matcher, Build, Context, Path, RefPath,
                         realize_inplace, rmref, fsinit, pack, unpack, allrrefs,
                         gc, redefine, match_some, match_latest, dirrm,
                         mksettings, readstr, writestr)
-
 from typing import List, Optional
-
 from re import search
+from time import sleep
 import re
+import os
+from subprocess import Popen, PIPE
+from select import select
 
 Chunk=List[str]
 
 def scanmd(fpath:str)->List[Chunk]:
-  acc=[]; acc2=[]; inchunk=False
+  acc:list=[]; acc2:list=[]; inchunk=False
   for line in open(fpath).readlines():
     if inchunk:
       if search('^\s*```', line):
@@ -30,5 +32,45 @@ def scanmd(fpath:str)->List[Chunk]:
       else:
         pass
   return acc2
+
+def readout(fdr, prompt:bytes=b'>>>',timeout:Optional[int]=10)->str:
+  acc:bytes=b''
+  while select([fdr],[],[],timeout)[0] != []:
+    r=os.read(fdr, 1024);
+    if r==b'':
+      print('readout retval', acc.decode('utf-8').replace("\n","|"))
+      return acc.decode('utf-8')
+    print('... readout got', r.decode('utf-8').replace("\n","|"))
+    acc+=r
+    if search(prompt,acc,re.MULTILINE):
+      print('readout match', acc.decode('utf-8').replace("\n","|"))
+      return acc.decode('utf-8')
+  print('readout timeout')
+  return acc.decode('utf-8',acc.decode('utf-8').replace("\n","|"))
+
+
+def interact(fdr, fdw, text:str, prompt:str='>>>')->str:
+  print('Sending return')
+  os.write(fdw,'\n\n\n'.encode())
+  print('Waiting for prompt')
+  readout(fdr)
+  print('Sending the message')
+  os.write(fdw,text.encode())
+  os.write(fdw,'\n'.encode())
+  print('Reading the answer')
+  res=readout(fdr)
+  print('Done')
+  return res
+
+def run():
+  fdr=os.open('out.pipe', os.O_RDONLY | os.O_NONBLOCK | os.O_SYNC)
+  fdw=os.open('inp.pipe', os.O_WRONLY | os.O_SYNC)
+  # interact(fdr,fdw,'3+2')
+  interact(fdr,fdw,'3+2')
+
+# def interact(fpipe:str, chunk:Chunk)->None:
+#   with open(fpipe,'w') as w:
+#     w.write(
+#   with open(fpipe,'r') as r:
 
 
--- docs/demos/MDRUN.sh
+++ docs/demos/MDRUN.sh
@@ -9,18 +9,32 @@ mkfifo inp.pipe
 mkfifo out.pipe
 
 
-python -i -c 'print("aaaaaaa")' <inp.pipe >inp.pipe &
+python -u -i -c 'print("HELLOOO")' <inp.pipe >out.pipe 2>&1 &
 # python -i -c 'print("aaaaaaa")' <inp.pipe &
 pid=$!
+sleep 999999999 >out.pipe &
+pid2=$!
+sleep 999999999 >inp.pipe &
+pid3=$!
+
 echo "Pid $pid"
-trap "kill $pid" EXIT
+trap "kill $pid $pid2 $pid3" EXIT
 
-socat -ddd READLINE PIPE:inp.pipe
-echo "Re-running socat"
-socat -ddd READLINE PIPE:inp.pipe
+bash
 
-# bash
+# echo "HERE"
+# exec 3>inp.pipe 4<out.pipe
+# echo "THERE"
+# socat -dd STDIN!!STDOUT FD:4!!FD:3
+# exec 3>&-
+# exec 4>&-
 
+
+# echo "Re-running socat"
+# echo "HERE"
+# exec 5>inp.pipe 6<out.pipe
+# echo "THERE"
+# socat -dd STDIN!!STDOUT FD:6!!FD:5
 
 
 
