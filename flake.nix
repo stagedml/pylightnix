5d90f0134a2d6ada69222988a2a830d13c9e9dd1
Sergei Mironov <grrwlf@gmail.com>
2022-04-10 23:52:24 +0400

    Add flake.nix and the lockfile

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Pylightnix flake";
+
+  inputs.nixpkgs.url = github:grwlf/nixpkgs/local12;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      let
+        python = python38;
+        doCheck = true;
+        version = "0.3";
+      in
+      with python.pkgs;
+      buildPythonPackage {
+        pname = "pylightnix";
+        inherit version;
+        src = builtins.filterSource (
+          path: type: !( baseNameOf path == "build" && type == "directory" ) &&
+                      !( baseNameOf path == "dist" && type == "directory" ) &&
+                      !( baseNameOf path == "result" )
+          ) ./.;
+
+        preConfigure = ''
+          export PATH="${atool}/bin:${curl}/bin:${wget}/bin:${git}/bin:$PATH"
+        '';
+
+        buildInputs = [ setuptools_scm ];
+
+        checkInputs = [ pytest pytest-mypy hypothesis pytest_xdist];
+
+        SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+        checkPhase = ''
+          export PATH=\
+          "${atool}/bin:${curl}/bin:${wget}/bin:"\
+          "${zip}/bin:${unzip}/bin:$PATH"
+          pytest -n `nproc`
+        '';
+
+        inherit doCheck;
+      };
+  };
+}
