7bb21944b93f13b75a0eacc81017e5c96a0fea05
Sergei Mironov <grrwlf@gmail.com>
2024-02-25 21:27:37 +0400

    env: Update

--- default.nix
+++ default.nix
@@ -1,40 +1,111 @@
-{ pkgs ?  import <nixpkgs> {}
-, python ? pkgs.python37
-, doCheck ? true
+{ nixpkgs ?  import <nixpkgs> {}
+, src ? builtins.filterSource (
+        path: type: !( baseNameOf path == "build" && type == "directory" ) &&
+                    !( baseNameOf path == "dist" && type == "directory" ) &&
+                    !( baseNameOf path == "result" )
+        ) ./.
 } :
 
-with python.pkgs;
-buildPythonPackage {
-  pname = "pylightnix";
-  version = "9999";
-  src = builtins.filterSource (
-    path: type: !( baseNameOf path == "build" && type == "directory" ) &&
-                !( baseNameOf path == "dist" && type == "directory" ) &&
-                !( baseNameOf path == "result" )
-    ) ./.;
+let
 
-  preConfigure = ''
-    export PATH="${pkgs.atool}/bin:${pkgs.curl}/bin:${pkgs.wget}/bin:${pkgs.git}/bin:$PATH"
-    if ! test -d /build/pylightnix/.git ; then
-      echo "Looks like Pylightnix is a submodule of some other repo."\
-           "\`nix-build\` is unable to detect its version, unfortunately."\
-           "Please checkout Pylightnix as a normal Git repository"\
-           "and retry." >&2
-      exit 1
-    fi
-  '';
+  python = nixpkgs.python3;
 
-  buildInputs = [ setuptools_scm ];
+  local = rec {
 
-  checkInputs = [ pytest pytest-mypy hypothesis ];
+    inherit (nixpkgs) atool curl wget git;
 
-  checkPhase = ''
-    export PATH=\
-    "${pkgs.atool}/bin:${pkgs.curl}/bin:${pkgs.wget}/bin:"\
-    "${pkgs.zip}/bin:${pkgs.unzip}/bin:$PATH"
-    pytest
-  '';
+    python-dev = nixpkgs.python3.withPackages (
+      pp: let
+        pylsp = pp.python-lsp-server;
+        pylsp-mypy = pp.pylsp-mypy.override { python-lsp-server=pylsp; };
+      in with pp; [
+      setuptools
+      setuptools_scm
+      ipython
+      hypothesis
+      pytest
+      pytest-mypy
+      pytest-cov
+      pytest_xdist
+      # Pweave
+      coverage
+      pylsp
+      pylsp-mypy
+      pyyaml
+      wheel
+      scipy
+      matplotlib
+      pyqt5
+      twine
 
-  inherit doCheck;
-}
+      # (pydoc-markdown pp)
+      codecov
+    ]);
 
+
+    pylightnix = with python.pkgs; buildPythonPackage {
+      pname = "pylightnix";
+      version = "9999";
+      inherit src;
+
+      preConfigure = ''
+        export PATH="${atool}/bin:${curl}/bin:${wget}/bin:${git}/bin:$PATH"
+        if ! test -d /build/pylightnix/.git ; then
+          echo "Looks like Pylightnix is a submodule of some other repo."\
+               "\`nix-build\` is unable to detect its version, unfortunately."\
+               "Please checkout Pylightnix as a normal Git repository"\
+               "and retry." >&2
+          exit 1
+        fi
+      '';
+
+      buildInputs = [ setuptools_scm ];
+
+      checkInputs = [ pytest pytest-mypy hypothesis ];
+
+      checkPhase = ''
+        export PATH=\
+        "${pkgs.atool}/bin:${pkgs.curl}/bin:${pkgs.wget}/bin:"\
+        "${pkgs.zip}/bin:${pkgs.unzip}/bin:$PATH"
+        pytest
+      '';
+
+      doCheck = true;
+    };
+
+    shell = nixpkgs.mkShell {
+      name = "shell";
+      buildInputs = with nixpkgs; [
+        gnumake
+        cloc
+        python-dev
+
+        (let
+           mytexlive = texlive.override { python3=python-dev; };
+         in
+           mytexlive.combine {
+             scheme-medium = mytexlive.scheme-medium;
+             inherit (mytexlive) fvextra upquote xstring pgfopts currfile
+             collection-langcyrillic makecell ftnxtra minted catchfile framed
+             pdflscape environ trimspaces mdframed zref needspace import;
+           }
+        )
+      ];
+
+      shellHook = with nixpkgs; ''
+        export PYTHONPATH=`pwd`/src:$PYTHONPATH
+        export MYPYPATH=`pwd`/src:`pwd`/tests
+        export QT_QPA_PLATFORM_PLUGIN_PATH=`echo ${qt5.qtbase.bin}/lib/qt-*/plugins/platforms/`
+        # 1980 workaround https://github.com/NixOS/nixpkgs/issues/270#issuecomment-467583872
+        export SOURCE_DATE_EPOCH=315532800
+        alias ipython="sh $(pwd)/ipython.sh"
+      '';
+    };
+
+    collection = {
+      inherit pylightnix shell;
+    };
+
+  };
+in
+  local.collection
--- flake.nix
+++ flake.nix
@@ -2,47 +2,24 @@
   description = "Pylightnix flake";
 
   inputs = {
-    # nixpkgs.url = github:grwlf/nixpkgs/local12;
+    # nixpkgs.url = github:grwlf/nixpkgs/local15;
     nixpkgs.url = "path:/home/grwlf/proj/nixcfg/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      let
-        python = python3;
-        doCheck = true;
-        version = "0.3";
-      in
-      with python.pkgs;
-      buildPythonPackage {
-        pname = "pylightnix";
-        inherit version;
-        src = builtins.filterSource (
-          path: type: !( baseNameOf path == "build" && type == "directory" ) &&
-                      !( baseNameOf path == "dist" && type == "directory" ) &&
-                      !( baseNameOf path == "result" )
-          ) ./.;
-
-        preConfigure = ''
-          export PATH="${atool}/bin:${curl}/bin:${wget}/bin:${git}/bin:$PATH"
-        '';
-
-        buildInputs = [ setuptools_scm ];
-
-        checkInputs = [ pytest pytest-mypy hypothesis pytest_xdist];
-
-        SETUPTOOLS_SCM_PRETEND_VERSION = version;
-
-        checkPhase = ''
-          export PATH=\
-          "${atool}/bin:${curl}/bin:${wget}/bin:"\
-          "${zip}/bin:${unzip}/bin:$PATH"
-          pytest -n `nproc`
-        '';
-
-        inherit doCheck;
+  outputs = { self, nixpkgs }:
+    let
+      defaults = (import ./default.nix) {
+        inherit nixpkgs;
+        src = self;
       };
-  };
+    in {
+      packages = {
+        x86_64-linux = defaults;
+      };
+      devShells = {
+        x86_64-linux = {
+          default = defaults.shell;
+        };
+      };
+    };
 }
